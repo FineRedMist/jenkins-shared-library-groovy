dc33adbd148d04ba3540c2122b2a65687f7a828f
Brent Scriver <bscriver@oneoddsock.com>
2022-12-19 12:03:12 -0500

    Add a simple generic exec for shell commands.

--- src/org/sample/jenkins/Generic.groovy
+++ src/org/sample/jenkins/Generic.groovy
@@ -1,10 +1,22 @@
 package org.sample.jenkins
 
 import org.jenkinsci.plugins.workflow.cps.CpsScript
+import org.sample.jenkins.NodeInfo
 
 import groovy.xml.*
 
 class Generic {
+    /**
+     * Simple batch file to execute a script based on the platform of the node.
+     */
+    static def exec(CpsScript script, String cmd, String encoding = null, String label = null, boolean returnStatus = false, boolean returnStdout = false) {
+        if(NodeInfo.isUnix(script)) {
+            return script.sh script: "#!/bin/sh\n${cmd}", encoding: encoding, label: label, returnStatus: returnStatus, returnStdout: returnStdout
+        } else {
+            return script.bat script: cmd, encoding: encoding, label: label, returnStatus: returnStatus, returnStdout: returnStdout
+        }
+    }
+
     static String readTextFile(CpsScript script, String filePath) {
         def bin64 = script.readFile(file: filePath, encoding: 'Base64')
         def binDat = bin64.decodeBase64()
