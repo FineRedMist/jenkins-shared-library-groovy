24431e493abd7ed8c693889c5f41a88692848f43
Brent Scriver <bscriver@oneoddsock.com>
2022-12-31 20:55:53 -0500

    Add artifacts param

--- src/org/sample/jenkins/CSharpBuilder.groovy
+++ src/org/sample/jenkins/CSharpBuilder.groovy
@@ -60,7 +60,7 @@ class CSharpBuilder {
                     notifyBuildStatus(BuildNotifyStatus.Failure)
                 } finally {
                     script.echo('Archiving artifacts...')
-                    script.archiveArtifacts('logs/**', allowEmptyArchive=true, onlyIfSuccessful=false)
+                    script.archiveArtifacts(artifacts='logs/**', allowEmptyArchive=true, onlyIfSuccessful=false)
                     script.cleanWs()
                 }
             }
